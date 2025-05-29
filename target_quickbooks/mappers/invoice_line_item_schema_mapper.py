a824599fee52d12923bf1929d824aee1b4647608
Vinicius Ghellere Sbardelotto <vinicius@hotglue.io>
2025-05-29 19:36:38 -0300

    feat: update invoice line item schema mapper

--- target_quickbooks/mappers/invoice_line_item_schema_mapper.py
+++ target_quickbooks/mappers/invoice_line_item_schema_mapper.py
@@ -1,18 +1,113 @@
 from typing import Dict
-from target_quickbooks.mappers.base_mapper import BaseMapper, RecordNotFound
+from target_quickbooks.mappers.base_mapper import BaseMapper, InvalidInputError, RecordNotFound
 
 class InvoiceLineItemSchemaMapper(BaseMapper):
     existing_record_pk_mappings = []
 
     field_mappings = {
-        "": ""
+        "description": "Description"
     }
 
     def to_quickbooks(self) -> Dict:
         payload = {
-            
+            **self._map_sales_item_line_details()
         }
 
         self._map_fields(payload)
 
         return payload
+
+    def _map_sales_item_line_details(self):
+        details_info = {
+            "DetailType": "SalesItemLineDetail",
+            "SalesItemLineDetail": {
+                **self._map_item(),
+                **self._map_line_tax_code(),
+                **self._map_class()
+            }
+        }
+
+        field_mappings = {
+            "discount": "DiscountAmt",
+            "quantity": "Qty",
+            "unitPrice": "UnitPrice",
+            "serviceDate": "ServiceDate"
+        }
+
+        self._map_fields(details_info["SalesItemLineDetail"], custom_field_mappings=field_mappings)
+
+        details_info["Amount"] = details_info["SalesItemLineDetail"].get("Qty", 1) * details_info["SalesItemLineDetail"].get("UnitPrice", 0)
+
+        return details_info
+    
+    def _map_item(self):
+        item_info = {}
+        found_item = None
+
+        if item_id := self.record.get("itemId"):
+            found_item = next(
+                (item for item in self.reference_data["Items"]
+                if item["Id"] == item_id),
+                None
+            )
+
+        if (item_name := self.record.get("itemName")) and not found_item:
+            found_item = next(
+                (item for item in self.reference_data["Items"]
+                if item["Name"] == item_name),
+                None
+            )
+
+        if (item_id or item_name) and found_item is None:
+            raise RecordNotFound(f"An item with Id={item_id} / Name={item_name} could not be found in QBO")
+
+        if found_item:
+            item_info["ItemRef"] = {
+                "value": found_item["Id"],
+                "name": found_item["Name"]
+            }
+
+        return item_info
+
+    def _map_class(self):
+        class_info = {}
+        found_class = None
+
+        if class_id := self.record.get("classId"):
+            found_class = next(
+                (_class for _class in self.reference_data["Classes"]
+                if _class["Id"] == class_id),
+                None
+            )
+
+        if (class_name := self.record.get("className")) and not found_class:
+            found_class = next(
+                (_class for _class in self.reference_data["Classes"]
+                if _class["Name"] == class_name),
+                None
+            )
+
+        if (class_id or class_name) and found_class is None:
+            raise RecordNotFound(f"A Class with Id={class_id} / Name={class_name} could not be found in QBO")
+
+        if found_class:
+            class_info["ClassRef"] = {
+                "value": found_class["Id"],
+                "name": found_class["Name"]
+            }
+
+        return class_info
+
+    def _map_line_tax_code(self):
+        tax_code_info = {}
+
+        if tax_code := self.record.get("taxCode"):
+            if tax_code not in ["TAX", "NON"]:
+                raise InvalidInputError(f"Invalid value {tax_code} for line taxCode, it should be either 'TAX' or 'NON'")
+
+            tax_code_info["TaxCodeRef"] = {
+                "value": tax_code,
+                "name": tax_code
+            }
+
+        return tax_code_info
