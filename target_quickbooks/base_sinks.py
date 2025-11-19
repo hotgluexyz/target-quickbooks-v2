df150a7944e47781b4094c397b4a143de2e90915
Key <keynarafaelp@gmail.com>
2025-11-19 17:06:52 -0500

    modify logs

--- target_quickbooks/base_sinks.py
+++ target_quickbooks/base_sinks.py
@@ -68,7 +68,7 @@ class QuickbooksBatchSink(HotglueBatchSink):
         # Update the latest state for each state update in the response
         self.logger.info(f"Updating state for {len(state_updates)} records")
         import os
-        self.logger.info(f"ALL os env: {os.environ}")
+        self.logger.info(f"ALL os env 2: {os.environ}")
         for i, state_update in enumerate(state_updates):
             self.update_state(state_update, record=records[i])
 
